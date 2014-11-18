4d21a6ae189ffd321ade119cd1ab16b8fed80ead
liecj3 <liecj3@bfh.ch>
2014-11-18 14:35:06 +0100

    fix typo

--- webshop/views/products.php
+++ webshop/views/products.php
@@ -7,7 +7,7 @@ foreach ($products as $product) {?>
 	<div class="product" name="<?php echo $product->id ?>">
 		<span class="title"><?php echo $product->name ?></span>
 		<a href=./index.php?view=detail&id=<?php echo $product->id ?>>
-			<img clas="productimage" src="./img/AppenzellerBrandloescher_Flasche.png" style="width:50px;">
+			<img class="productimage" src="./img/AppenzellerBrandloescher_Flasche.png" style="width:50px;">
 		</a>
 		<span class="price"><?php echo $product->price ?></span>
 		<form action="cart.php" method="post">
