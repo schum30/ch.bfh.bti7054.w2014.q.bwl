e428583485709c9ba39cf29a3de8da8922c96882
liecj3 <liecj3@bfh.ch>
2014-12-02 16:01:21 +0100

    updaet product_info.php and products.php to display the productImage

--- webshop/views/product_info.php
+++ webshop/views/product_info.php
@@ -1,15 +1,13 @@
 <?php 
-$id = isset($_GET["id"]) ? $_GET["id"] : NULL;
-$productDB = new ProductDB();
-$product = $productDB->getProduct($id);
+$dbHandler = new dbHandler();
+$product = isset($_GET["id"]) ? $dbHandler->getProduct($_GET["id"]) : NULL;
 ?>
 <div class="product">
 	<ul>
-		<li><img src="./img/AppenzellerBrandloescher_Flasche.png">
+		<li><img src="<?php echo $product->imgPath ?>">
 		<li><?php echo "$expr[name]: $product->name" ?></li>
 		<li><?php echo "$expr[price]: $product->price" ?></li>
 		<li>Amber, süsslich-herb, üppige Karamalznote, mittelkräftig und cremig</li>
-		<li><?php echo "ID: $product->id"?></li>
 		<li><a href="javascript:addToCart(<?php echo $id ?>)">add to cart!</a></li>
 	</ul>
 	<a href="./index.php">Zurück</a>
--- webshop/views/products.php
+++ webshop/views/products.php
@@ -6,7 +6,7 @@ foreach ($products as $product) {?>
 	<div class="product" name="<?php echo $product->id ?>">
 		<span class="title"><?php echo $product->name ?></span>
 		<a href=./index.php?view=detail&id=<?php echo $product->id ?>>
-			<img class="productimage" src="./img/AppenzellerBrandloescher_Flasche.png" style="width:50px;">
+			<img class="productimage" src="<?php echo $product->imgPath ?>" style="width:50px;">
 		</a>
 		<span class="price"><?php echo $product->price ?></span>
 		<form action="cart.php" method="post">
